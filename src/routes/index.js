056697b25e73d06e03078fe020b8a5645ae1f710
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2020-06-16 22:33:33 +0100

    (ft): setup

---
+++ src/routes/index.js
@@ -0,0 +1,56 @@
+/* -------------------------------------------------------------------------- */
+/*                            External Dependencies                           */
+/* -------------------------------------------------------------------------- */
+import React from 'react';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import styled from 'styled-components';
+
+/* -------------------------- Internal Dependencies ------------------------- */
+import Home from 'pages';
+import ErrorBoundary from 'components/error-boundary';
+
+/* ---------------------------- Routes PropTypes ---------------------------- */
+const propTypes = {
+	location: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
+};
+
+const Routes = ({ location }) => (
+	<Wrapper>
+		<ErrorBoundary>
+			<TransitionGroup>
+				<CSSTransition
+					key={location.key}
+					timeout={{ enter: 300, exit: 300 }}
+					classNames="fade"
+				>
+					<Switch location={location}>
+						<Route exact path="/" component={Home} />
+					</Switch>
+				</CSSTransition>
+			</TransitionGroup>
+		</ErrorBoundary>
+	</Wrapper>
+);
+
+const Wrapper = styled.div`
+	.fade-enter {
+		opacity: 0.6;
+	}
+	.fade-enter.fade-enter-active {
+		opacity: 1;
+		transition: opacity 0.4s ease-in;
+	}
+	.fade-exit {
+		opacity: 1;
+	}
+	.fade-exit.fade-exit-active {
+		opacity: 0.6;
+		transition: opacity 0.4s ease-in;
+	}
+`;
+
+Routes.propTypes = propTypes;
+
+export default withRouter(Routes);
